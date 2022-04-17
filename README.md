c9fe16521a75364b2a23ff8709e63f46c76f1f6c
lempiji <lempiji@gmail.com>
2022-04-17 16:48:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fabric
+
+## Overview
+
+This is a bundle of utilities.
+
+- Some extend the `std`
+- Intuitive to use
+- Easy to use
+
+## License
+
+This library is under the MIT License.  
+Some code is borrowed from [Rx.NET](https://github.com/Reactive-Extensions/Rx.NET).
---
+++ source/fabric/sumtype.d
@@ -0,0 +1,27 @@
+module fabric.sumtype;
+
+public import std.sumtype;
+
+template matchAny(Types...)
+{
+    bool matchAny(TSumType)(auto ref TSumType obj)
+    if (isSumType!TSumType)
+    {
+        import std.meta : staticMap;
+
+        alias matchHandler(T) = (in T _) => true;
+
+        return obj.match!(staticMap!(matchHandler, Types), _ => false);
+    }
+}
+
+unittest
+{
+    SumType!(int, string, Object) obj = 100;
+
+    assert(obj.matchAny!int);
+    assert(obj.matchAny!(int, string));
+
+    assert(!obj.matchAny!string);
+    assert(!obj.matchAny!(string, Object));
+}
